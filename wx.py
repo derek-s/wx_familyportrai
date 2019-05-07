38d257336a8b05e3a6c8c85c881d5dc7a341bbf7
Derek.S <derekseli@outlook.com>
2019-05-07 09:37:30 +0800

    weixin login and get head image

---
+++ wx.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/6 16:40
+# @Author  : Derek.S
+# @Site    : 
+# @File    : wx.py
+
+import itchat
+
+
+def getHeadImg():
+    """
+    login weixin, get friends lists, download friends head image
+    :return: None
+    """
+    itchat.auto_login(hotReload=True)
+    friends = itchat.get_friends()
+    for item in friends:
+        img = itchat.get_head_img(userName=item["UserName"])
+
+        fileImg = open("headimg/" + str(item["UserName"]) + ".jpg", "wb")
+        fileImg.write(img)
+        fileImg.close()
+
