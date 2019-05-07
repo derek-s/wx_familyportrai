15d81d9910e365b51caf092a2894253f39794be1
Derek.S <derekseli@outlook.com>
2019-05-07 10:58:58 +0800

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/6 16:41
+# @Author  : Derek.S
+# @Site    : 
+# @File    : main.py
+
+from plece import plece
+from wx import getHeadImg
+
+if __name__ == "__main__":
+    getHeadImg()
+    plece()
